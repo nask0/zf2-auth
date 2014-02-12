f43fbccb0dcaa4c250dd0d438b151fb2e13132e0
Atanas Belobodov <arrtedone@gmail.com>
2014-02-12 11:58:59 +0200

    changes

--- Module.php
+++ Module.php
@@ -23,9 +23,7 @@ class Module
 
     public function getConfig()
     {
-        return
-            (include __DIR__ . '/config/module.config.php') +
-            (include __DIR__ . '/config/routes.config.php');
+        return (include __DIR__ . '/config/module.config.php');
     }
 
     public function getAutoloaderConfig()
